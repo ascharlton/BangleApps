274f5443d741130deb16e34ba86ffe2c5ec7cd9a
ascharlton <71233363+ascharlton@users.noreply.github.com>
2020-09-14 01:10:19 +0100

    Update clock-morphing.js

--- apps/mclock/clock-morphing.js
+++ apps/mclock/clock-morphing.js
@@ -152,7 +152,7 @@ function drawEverythingElse() {
   var y = Y + 2*CHARW + CHARP;
   var d = new Date();
   g.reset();
-  g.setFont("6x8");
+  g.setFont("6x8",2);
   g.setFontAlign(-1,-1);
   g.drawString(("0"+d.getSeconds()).substr(-2), x, y-8, true);
   // meridian
